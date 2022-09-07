f8415f06cb8bf57f378d3884ef2fb5eece2bc652
Ali Sadik <aligsr@yahoo.com>
2022-09-07 10:28:55 -0400

    edit and add files

--- green/myCapstone_helloworld.py
+++ green/myCapstone_helloworld.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def myCapstone_helloworld():
-    message= "This my Capstone project at Udacity, Cloud DevOps Engineer Nanodegree!"
+    message = "This my Capstone project at Udacity, Cloud DevOps Engineer Nanodegree!"
     return message
-	
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
